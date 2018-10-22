9c977d6fdf1e070a9212278fbb35a575aa68142e
bpourian <b.pourian@hotmail.co.uk>
2018-10-22 20:11:44 +0100

    update

--- main.js
+++ main.js
@@ -9,10 +9,24 @@ var UI = null;
 
 function preload() {
   console.log("👉 Preload");
+  RAW_DATA = loadTable("data.csv", "csv", "header");
 }
 
 function setup() {
   console.log("👉 Setup");
+  console.log(RAW_DATA);
+  prepareData();
+
+}
+
+function prepareData() {
+    console.log("prepareData");
+    DATA = new Data(RAW_DATA);
+    console.log(DATA.training);
+}
+
+function validateModel() {
+  log
 }
 
 function draw() {}
